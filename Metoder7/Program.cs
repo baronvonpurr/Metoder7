002b0ae6e96e976f87833b0935ca579d889639e4
baronvonpurr <robinhood.svensson@hotmail.com>
2024-03-01 18:22:27 +0100

    Add project files.

---
+++ Metoder7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Metoder7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            Console.WriteLine(calculator(double.Parse(Console.ReadLine())));
+        }
+
+        static string calculator(double input)
+        {
+            double define = input / 2;
+
+            for (int i = 1; i < define; i++)
+            {
+                if (input % 2 == 0)
+                {
+                    return "Talet är inte ett Primtal";
+                }
+                else
+                {
+                    return "Talet är ett Primtal";
+                }
+            }
+            return "AAAAA";
+        }
+    }
+}
